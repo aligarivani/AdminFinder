8c76bbd13b583e7d01898b83fa778c269245915c
Ali Garivani <72452146+aligarivani@users.noreply.github.com>
2022-12-03 18:02:46 +0330

    add file primery

---
+++ AdminFinder.py
@@ -0,0 +1,77 @@
+# Created By Ali Garivani
+
+# import request for check address
+import requests
+# colorama for chenge Color Text in console
+from colorama import Fore, init
+
+init()
+
+casper = '''
+    _..gggggppppp.._                       
+                  _.gd$$$$$$$$$$$$$$$$$$bp._                  
+               .g$$$$$$P^^""j$$b""""^^T$$$$$$p.               
+            .g$$$P^T$$b    d$P T;       ""^^T$$$p.            
+          .d$$P^"  :$; `  :$;                "^T$$b.          
+        .d$$P'      T$b.   T$b                  `T$$b.        
+       d$$P'      .gg$$$$bpd$$$p.d$bpp.           `T$$b       
+      d$$P      .d$$$$$$$$$$$$$$$$$$$$bp.           T$$b      
+     d$$P      d$$$$$$$$$$$$$$$$$$$$$$$$$b.          T$$b     
+    d$$P      d$$$$$$$$$$$$$$$$$$P^^T$$$$P            T$$b    
+   d$$P    '-'T$$$$$$$$$$$$$$$$$$bggpd$$$$b.           T$$b   
+  :$$$      .d$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$p._.g.     $$$;  
+  $$$;     d$$$$$$$$$$$$$$$$$$$$$$$P^"^T$$$$P^^T$$$;    :$$$  
+ :$$$     :$$$$$$$$$$$$$$:$$$$$$$$$_    "^T$bpd$$$$,     $$$; 
+ $$$;     :$$$$$$$$$$$$$$bT$$$$$P^^T$p.    `T$$$$$$;     :$$$ 
+:$$$      :$$$$$$$$$$$$$$P `^^^'    "^T$p.    lb`TP       $$$;
+:$$$      $$$$$$$$$$$$$$$              `T$$p._;$b         $$$;
+$$$;      $$$$$$$$$$$$$$;                `T$$$$:Tb        :$$$
+$$$;      $$$$$$$$$$$$$$$                        Tb    _  :$$$
+:$$$     d$$$$$$$$$$$$$$$.                        $b.__Tb $$$;
+:$$$  .g$$$$$$$$$$$$$$$$$$$p...______...gp._      :$`^^^' $$$;
+ $$$;  `^^'T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$p.    Tb._, :$$$ 
+ :$$$       T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$b.   "^"  $$$; 
+  $$$;       `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$b      :$$$  
+  :$$$        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$;     $$$;  
+   T$$b    _  :$$`$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$;   d$$P   
+    T$$b   T$g$$; :$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  d$$P    
+     T$$b   `^^'  :$$ "^T$$$$$$$$$$$$$$$$$$$$$$$$$$$ d$$P     
+      T$$b        $P     T$$$$$$$$$$$$$$$$$$$$$$$$$;d$$P      
+       T$$b.      '       $$$$$$$$$$$$$$$$$$$$$$$$$$$$P       
+        `T$$$p.   bug    d$$$$$$$$$$$$$$$$$$$$$$$$$$P'        
+          `T$$$$p..__..g$$$$$$$$$$$$$$$$$$$$$$$$$$P'          
+            "^$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$^"            
+               "^T$$$$$$$$$$$$$$$$$$$$$$$$$$P^"               
+                   """^^^T$$$$$$$$$$P^^^"""
+'''
+
+print(casper)
+http = input(Fore.CYAN+' http ' + Fore.WHITE+'enter'+Fore.CYAN+" 1 " + Fore.WHITE +
+             'or'+Fore.GREEN+' https ' + Fore.WHITE+'enter'+Fore.GREEN+" 2 "+Fore.WHITE+':  ')
+
+if http == '1':
+    http = 'http'
+elif http == '2':
+    http = 'https'
+
+address = input("enter address site "+Fore.GREEN +
+                "('google.come')"+Fore.WHITE+" : ")
+
+admin_list = ['admin/', 'administrator/', 'login.php', 'administration/', 'admin1/', 'admin2/', 'admin3/', 'admin4/', 'admin5/', 'moderator/', 'webadmin/', 'adminarea/', 'bb-admin/', 'adminLogin/', 'admin_area/', 'panel-administracion/', 'instadmin/',
+              'memberadmin/', 'administratorlogin/', 'adm/', 'account.asp', 'admin/account.asp', 'admin/index.asp', 'admin/login.asp', 'admin/admin.asp', '/login.aspx',
+              'admin_area/admin.asp', 'admin_area/login.asp', 'admin/account.html', 'admin/index.html', 'admin/login.html', 'admin/admin.html',
+              'admin_area/admin.html', 'admin_area/login.html', 'admin_area/index.html', 'admin_area/index.asp', 'bb-admin/index.asp', 'bb-admin/login.asp', 'bb-admin/admin.asp',
+              'bb-admin/index.html']
+
+for item in admin_list:
+    url = requests.get(f'{http}://{address}/{item}').status_code
+
+    if url == 200:
+        print(Fore.GREEN + f'{http}://{address}/{item} ',
+              Fore.GREEN+str(" admin page True :) "))
+    elif url == 402:
+        print(Fore.RED + f'{http}://{address}/{item} ',
+              Fore.YELLOW+' acsess denied')
+    elif url == 404:
+        print(Fore.RED + f'{http}://{address}/{item} ',
+              Fore.RED + ' this addresss not True :( ')
